8e1df9991183e2950a679100a6152b2a734f9edd
Jerry457 <2234834467@qq.com>
2025-12-11 01:34:40 +0800

    Add snowman decorations  customize animation interface

--- modmain.lua
+++ modmain.lua
@@ -1,38 +1,127 @@
 local extra_decorations = {
-    ["watermelon_cooked"] = { canflip = true },
-    ["asparagus_cooked"] = { canflip = false },
+    watermelon_cooked = { canflip = true },
+    asparagus_cooked = { canflip = true },
+    nightmarefuel = {
+        bank = "nightmarefuel_decoration",
+        canflip = true,
+        custom_animation = {
+            use_point_filtering = true,
+            mult_colour = { 1, 1, 1, 0.5 },
+        },
+    }
 }
 
 Assets = {
     Asset("ANIM", "anim/item_rotate.zip"),
 }
-for prefab in pairs(extra_decorations) do
-    table.insert(Assets, Asset("ANIM", "anim/" .. prefab .. "_decoration.zip"))
+for prefab, data in pairs(extra_decorations) do
+    data.name = prefab
+    data.bank = data.bank or "item_rotate"
+    data.build = data.build or (prefab .. "_decoration")
+    data.anim = data.anim or "snowman_decor"
+
+    if data.custom_animation then
+        data.custom_animation.num_rots = data.custom_animation.num_rots or 16
+    end
+
+    table.insert(Assets, Asset("ANIM", "anim/" .. data.build .. ".zip"))
 end
 
 local AddPrefabPostInit = AddPrefabPostInit
 GLOBAL.setfenv(1, GLOBAL)
 
+local UpvalueUtil = require("upvalueutil")
+
+local SnowmanDecoratingScreen = require("screens/redux/snowmandecoratingscreen")
 local SnowmanDecoratable = require("components/snowmandecoratable")
-local _, ITEM_DATA = debug.getupvalue(SnowmanDecoratable.GetItemData, 1)
 
-if not ITEM_DATA then
-    return ITEM_DATA
-end
-
+local ITEM_DATA = UpvalueUtil.GetUpvalue(SnowmanDecoratable.GetItemData, "ITEM_DATA")
 for prefab, data in pairs(extra_decorations) do
     ITEM_DATA[hash(prefab)] = data
-    data.name = prefab
-    data.bank = "item_rotate"
-    data.build = prefab .. "_decoration"
-    data.anim = "snowman_decor"
     AddPrefabPostInit(prefab, function(inst)
         if not TheWorld.ismastersim then
             return
         end
-
         inst:AddComponent("snowmandecor")
     end)
 end
 
-TUNING.SNOWMAN_MAX_DECOR = {}
+local function UseCustomAnimation(itemdata, AnimState, flip, rot)
+    local custom_animation = itemdata.custom_animation
+    if not custom_animation then
+        return
+    end
+
+    local animation = itemdata.anim .. (flip and itemdata.canflip and "_flip_" or "_") .. (rot - 1)
+    AnimState:PlayAnimation(animation, true)
+    if custom_animation.mult_colour then
+        AnimState:SetMultColour(unpack(custom_animation.mult_colour))
+    end
+    if custom_animation.use_point_filtering then
+        AnimState:UsePointFiltering(true)
+    end
+
+    AnimState:Resume()
+end
+
+local _CreateDecor, i, _DoDecor = UpvalueUtil.GetUpvalue(SnowmanDecoratable.ApplyDecor, "_DoDecor.CreateDecor")
+local function CreateDecor(itemdata, rot, flip, ...)
+    local inst = _CreateDecor(itemdata, rot, flip, ...)
+    UseCustomAnimation(itemdata, inst.AnimState, flip, rot)
+
+    return inst
+end
+debug.setupvalue(_DoDecor, i, CreateDecor)
+
+local _StartDraggingItem = SnowmanDecoratingScreen.StartDraggingItem
+function SnowmanDecoratingScreen:StartDraggingItem(obj, ...)
+    _StartDraggingItem(self, obj, ...)
+    UseCustomAnimation(self.dragitem.itemdata, self.dragitem:GetAnimState(), self.dragitem.flip, self.dragitem.rot)
+end
+
+local _DoAddItemAt = SnowmanDecoratingScreen.DoAddItemAt
+function SnowmanDecoratingScreen:DoAddItemAt(x, y, itemhash, itemdata, rot, flip, ...) --snowball local space
+    local decor = _DoAddItemAt(self, x, y, itemhash, itemdata, rot, flip, ...)
+    UseCustomAnimation(itemdata, decor:GetAnimState(), flip, rot)
+    return decor
+end
+
+local _CanRotateDraggingItem = SnowmanDecoratingScreen.CanRotateDraggingItem
+function SnowmanDecoratingScreen:CanRotateDraggingItem(...)
+    if self.dragitem and self.dragitem.itemdata.custom_animation then
+        return self.dragitem ~= nil and self.dragitem.shown and self.dragitem.itemdata.custom_animation.num_rots > 1
+    end
+    return _CanRotateDraggingItem(self, ...)
+end
+
+local _RotateDraggingItem = SnowmanDecoratingScreen.RotateDraggingItem
+function SnowmanDecoratingScreen:RotateDraggingItem(delta, ...)
+    local itemdata = self.dragitem.itemdata
+    if self.dragitem and itemdata.custom_animation then
+        local num_rots = itemdata.custom_animation.num_rots
+        self.dragitem.rot = self.dragitem.rot + delta
+        while self.dragitem.rot > num_rots do
+            self.dragitem.rot = self.dragitem.rot - num_rots
+        end
+        while self.dragitem.rot < 1 do
+            self.dragitem.rot = self.dragitem.rot + num_rots
+        end
+        local animation = itemdata.anim .. (self.dragitem.flip and itemdata.canflip and "_flip_" or "_") .. (self.dragitem.rot - 1)
+        self.dragitem:GetAnimState():PlayAnimation(animation, true)
+    else
+        _RotateDraggingItem(self, delta, ...)
+    end
+end
+
+local _FlipDraggingItem = SnowmanDecoratingScreen.FlipDraggingItem
+function SnowmanDecoratingScreen:FlipDraggingItem(...)
+    local num_rots = self.dragitem.itemdata.custom_animation and self.dragitem.itemdata.custom_animation.num_rots or 1
+    local rot = ((num_rots - self.dragitem.rot + 1) % num_rots) + 1
+    _FlipDraggingItem(self, ...)
+    if self.dragitem and self.dragitem.itemdata.custom_animation then
+        self.dragitem.rot = rot
+        UseCustomAnimation(self.dragitem.itemdata, self.dragitem:GetAnimState(), self.dragitem.flip, self.dragitem.rot)
+    end
+end
+
+-- TUNING.SNOWMAN_MAX_DECOR = {}
