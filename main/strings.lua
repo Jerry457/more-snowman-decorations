6cb49910288243132266ca5b685c947037ed8748
Jerry457 <2234834467@qq.com>
2025-12-15 19:48:28 +0800

    Fix string missing

--- main/strings.lua
+++ main/strings.lua
@@ -1,4 +1,5 @@
 local modimport = modimport
+local AddSimPostInit = AddSimPostInit
 GLOBAL.setfenv(1, GLOBAL)
 
 local loc = require("languages/loc")
@@ -6,10 +7,11 @@ local loc = require("languages/loc")
 local function DoTranslate()
     local language = loc.GetLanguage and loc.GetLanguage() or nil
     if language == LANGUAGE.CHINESE_S or language == LANGUAGE.CHINESE_S_RAIL then
-        require("localization/snowman_strings_zh")
+        modimport("scripts/localization/snowman_strings_zh")
     else
-        require("localization/snowman_strings_en")
+        modimport("scripts/localization/snowman_strings_en")
     end
 end
 
 DoTranslate()
+AddSimPostInit(DoTranslate)
--- scripts/localization/snowman_strings_en.lua
+++ scripts/localization/snowman_strings_en.lua
@@ -1,3 +1,5 @@
+GLOBAL.setfenv(1, GLOBAL)
+
 STRINGS.SKIN_NAMES.snowball_item_dungball = "Dung ball"
 STRINGS.SKIN_DESCRIPTIONS.snowball_item_dungball = "This snowball’s got stinky vibes."
 
